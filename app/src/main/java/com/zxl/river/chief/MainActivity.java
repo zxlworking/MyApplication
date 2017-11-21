24b6d38a1fa79cb82e65c156f246350acd2601dc
zxlworking <szzhangxiaolong@163.com>
2017-11-21 09:18:55 +0800

    使用fragment pager
    Signed-off-by: zxlworking <szzhangxiaolong@163.com>

--- app/src/main/java/com/zxl/river/chief/MainActivity.java
+++ app/src/main/java/com/zxl/river/chief/MainActivity.java
@@ -2,14 +2,26 @@ package com.zxl.river.chief;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.zxl.river.chief.fragmetn.MapFollowFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends FragmentActivity {
 
     private ViewPager mViewPager;
+
+    private CustomAdapter mCustomAdapter;
+
+    private List<Fragment> mFragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +30,30 @@ public class MainActivity extends FragmentActivity {
 
         //addPreferencesFromResource(R.xml.category);
 
+        mFragments.add(new MapFollowFragment());
+
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mCustomAdapter = new CustomAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mCustomAdapter);
 
     }
 
-    class CustomAdapter extends PagerAdapter{
+    class CustomAdapter extends FragmentPagerAdapter{
+
+        public CustomAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
         @Override
         public int getCount() {
-            return 0;
+            return mFragments.size();
         }
 
         @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return false;
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
         }
+
     }
 
 }
